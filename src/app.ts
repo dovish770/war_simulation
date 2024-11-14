e699a5276afc8c0f79e1a07cd6cf3ed1ce747e98
dovi stein <dovish@gmail.com>
2024-11-14 15:07:52 +0200

    added socket

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,30 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import http from "http";
 import connectDb from "./config/MongoDb";
 import ConnectionRouter from "./routes/connection";
 import defence from "./routes/defenceRouter";
 import attack from "./routes/attackRouter";
-
+import { setupWebSocket } from "./sockets/webSocket";
 dotenv.config();
 
 
 const app = express();
 const PORT = process.env.PORT || 7770;
 
-// Middleware
-
-
 app.use(express.json());
 app.use(cors());
+
+const server = http.createServer(app);
+
+setupWebSocket(server);
 
 app.use('/war_simulation', ConnectionRouter, defence, attack)
 
 connectDb();
 
 
-// Error handling middleware
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
